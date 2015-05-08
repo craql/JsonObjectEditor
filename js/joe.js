35fd6990d1f9ac7a4f162268683950cfe2302910
Corey H <craql@live.com>
2015-05-08 10:51:41 -0700

    actually fixed list/ref

--- js/joe.js
+++ js/joe.js
@@ -2263,7 +2263,7 @@ this.renderSorterField = function(prop){
                 values:values,
                 skip:true,
                 name:prop.name,
-                ftype:'objectreference'
+                ftype:'objectReference'
 
             }
         ); //,{onblur:'_joe.showMessage($(this).val());'})
@@ -3022,8 +3022,8 @@ this.renderSorterField = function(prop){
         var required_missed = [];
         req_fields.map(function (f) {
             if (!obj[f.name] ||
-                ($.type(obj[f.name] == "array") && f.type != "objectlist" && obj[f.name].length == 0 ) ||
-                (f.type == "objectlist" && obj[f.name].filter(olistVal).length == 0)
+                ($.type(obj[f.name] == "array") && f.type != "objectList" && obj[f.name].length == 0 ) ||
+                (f.type == "objectList" && obj[f.name].filter(olistVal).length == 0)
             ) {
 
                 required_missed.push(f);
@@ -3164,7 +3164,7 @@ this.renderSorterField = function(prop){
 		//var parentFind = $('.joe-overlay.active');
 
 		parentFind.find('.joe-field').each(function(){
-            if($(this).parents('.objectlist-field').length){
+            if($(this).parents('.objectList-field').length){
                 return true;
             }
 			if(self.current.userSpecs.multiedit){
@@ -3229,10 +3229,10 @@ this.renderSorterField = function(prop){
                                  });*/
                                 object[prop] = vals;
                                 break;
-                            case 'objectreference':
+                            case 'objectReference':
                                 var vals = [];
-                                object[prop] = 'objectreference';
-                                $('.objectreference-field[data-name="'+prop+'"]').find('.joe-field-item')
+                                object[prop] = 'objectReference';
+                                $('.objectReference-field[data-name="'+prop+'"]').find('.joe-field-item')
                                     .each(function(){
                                         vals.push($(this).data().value);
                                     });
@@ -3249,7 +3249,7 @@ this.renderSorterField = function(prop){
 
 
     //OBJECT LISTS
-        parentFind.find('.objectlist-field').each(function(){
+        parentFind.find('.objectList-field').each(function(){
             var ol_property = $(this).data('name');
             var ol_array = [];
         //get table header rows
